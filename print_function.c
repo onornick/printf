50201b683152c09be6f683d450a8245e40702fa2
Nick <kiprotichnickson20@gmail.com>
2023-03-31 23:46:37 +0300

    edited betty style

--- print_function.c
+++ print_function.c
@@ -95,3 +95,21 @@ int print_percent(va_list l, flags_t *f)
  * Return: number of char printed
  */
 
+int print_int(va_list l, flags_t *f)
+{
+	int n = va_arg(l, int);
+	int res = count_digit(n);
+
+	if (f->space == 1 && f->plus == 0 && n >= 0)
+		res += _putchar(' ');
+
+	if (f->plus == 1 && n >= 0)
+		res += _putchar('+');
+
+	if (n <= 0)
+		res++;
+	print_number(n);
+
+	return (res);
+}
+
